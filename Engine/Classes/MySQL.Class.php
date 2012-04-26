4281cd35e7b966a411f3afe146b9c0eab8d6a9e6
electronrussia <bm.kot.urs@mail.ru>
2012-04-27 00:00:49 +0400

    Класс для работы с базами данных

---
+++ Engine/Classes/MySQL.Class.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * Класс для работы с базой данных MySQL
+ */
+
+class MySQL {
+    
+    # Функция подключения к серверу MySQL
+    function Connect($ServerMySQL, $UserMySQL, $PasswordMySQL) {
+        $Connect = @mysql_connect($ServerMySQL, $UserMySQL, $PasswordMySQL);
+        If ($Connect) {
+            return true;
+        } Else {
+            $this->Error = "1";
+            $this->ErrorText[] = "Не удалось подключиться к серверу баз данных!";
+            return false;
+        }
+    }
+    
+    # Функция подключения к БД MySQL
+    function Select($DataBaseMySQL) {
+        If ($this->Error == "1") {
+            $this->ErrorText[] = "Не удалось подключиться к базе данных!";
+            return false;
+        }
+        $Select = @mysql_select_db($DataBaseMySQL);
+        If ($Select) {
+            return true;
+        } Else {
+            $this->Error = "1";
+            $this->ErrorText[] = "Не удалось подключиться к базе данных!";
+            return false;
+        }
+    }
+    
+}
+
+?>
